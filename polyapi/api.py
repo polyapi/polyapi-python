2423b75edb4d5c40d0e629ee8ac9c018cf4008d9
Dan Fellin <dan@highwaterlabs.com>
2023-12-04 09:04:58 -0800

    woot getting there

--- polyapi/api.py
+++ polyapi/api.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 from polyapi.typedefs import PropertySpecification, PropertyType
 from polyapi.utils import append_init
 from polyapi.schema import generate_schema_types
@@ -15,21 +15,17 @@ import requests
 from typing import List, Dict, Any
 from polyapi.config import get_api_key_and_url
 from polyapi.exceptions import PolyApiException
-
-
-def {function_name}({args}) -> {return_type}:
+{args_def}
+{return_type_def}
+def {function_name}({args}) -> {return_type_name}:
     api_key, api_url = get_api_key_and_url()
-    return_str = {return_str}
     headers = {{"Authorization": f"Bearer {{api_key}}"}}
     url = f"{{api_url}}/functions/{function_type}/{function_id}/execute"
     data = {data}
     resp = requests.post(url, data=data, headers=headers)
     if resp.status_code != 200 and resp.status_code != 201:
         raise PolyApiException(f"{{resp.status_code}}: {{resp.content}}")
-    if return_str:
-        return resp.text
-    else:
-        return resp.json()
+    return {return_action}
 """
 
 
@@ -46,27 +42,35 @@ def _map_primitive_types(type_: str) -> str:
     return PRIMITIVE_TYPE_MAP.get(type_, "Any")
 
 
-def _get_type(type_spec: PropertyType) -> str:
+def _get_type(type_spec: PropertyType) -> Tuple[str, str]:
     if type_spec["kind"] == "plain":
-        return _map_primitive_types(type_spec["value"])
+        return _map_primitive_types(type_spec["value"]), ""
     elif type_spec["kind"] == "primitive":
-        return _map_primitive_types(type_spec["type"])
+        return _map_primitive_types(type_spec["type"]), ""
     elif type_spec["kind"] == "array":
-        # return f"List[{_get_type(type_spec['type'])}]"
-        return "List"
+        return "Responsetype", generate_schema_types(type_spec)  # type: ignore
     elif type_spec["kind"] == "void":
-        return "None"
+        return "None", ""
     elif type_spec["kind"] == "object":
-        return "Dict"
+        if type_spec.get("schema"):
+            return type_spec["schema"]["title"].title(), generate_schema_types(type_spec["schema"])  # type: ignore
+        else:
+            return "Dict", ""
     elif type_spec["kind"] == "any":
-        return "Any"
+        return "Any", ""
     else:
-        return "Any"
+        return "Any", ""
 
 
-def _get_arg_string(arguments: List[PropertySpecification]) -> str:
-    arg_strings = [f"{a['name']}: {_get_type(a['type'])}" for a in arguments]
-    return ", ".join(arg_strings)
+def _parse_arguments(arguments: List[PropertySpecification]) -> Tuple[str, str]:
+    args_def = []
+    arg_strings = []
+    for a in arguments:
+        arg_type, arg_def = _get_type(a["type"])
+        if arg_def:
+            args_def.append(arg_def)
+        arg_strings.append(f"{a['name']}: {arg_type}")
+    return ", ".join(arg_strings), "\n\n".join(args_def)
 
 
 def render_function(
@@ -77,19 +81,26 @@ def render_function(
     return_type: Dict[str, Any],
 ) -> str:
     arg_names = [a["name"] for a in arguments]
-    arg_string = _get_arg_string(arguments)
-    return_type_string = _get_type(return_type)  # type: ignore
-    return_str = return_type_string == "str"
+    args, args_def = _parse_arguments(arguments)
+    return_type_name, return_type_def = _get_type(return_type)  # type: ignore
     data = "{" + ", ".join([f"'{arg}': {arg}" for arg in arg_names]) + "}"
-    return TEMPLATE.format(
+    if return_type_def == "str":
+        return_action = "resp.text"
+    else:
+        return_action = "resp.json()"
+
+    rendered = TEMPLATE.format(
         function_type=TEMPLATE_FUNCTION_TYPE_MAP[function_type],
         function_name=function_name,
         function_id=function_id,
-        args=arg_string,
-        return_type=return_type_string,
-        return_str=return_str,
+        args=args,
+        args_def=args_def,
+        return_type_name=return_type_name,
+        return_type_def=return_type_def,
+        return_action=return_action,
         data=data,
     )
+    return rendered
 
 
 def add_function_file(
--- polyapi/schema.py
+++ polyapi/schema.py
@@ -6,24 +6,33 @@ import json
 
 
 def _temp_store_input_data(input_data: Dict) -> str:
-    """ take in the input data and store it in a temporary json file
-    """
-    with tempfile.NamedTemporaryFile(mode='w', delete=False, prefix="polyapi_", suffix=".json") as temp_file:
+    """take in the input data and store it in a temporary json file"""
+    with tempfile.NamedTemporaryFile(
+        mode="w", delete=False, prefix="polyapi_", suffix=".json"
+    ) as temp_file:
         json.dump(input_data, temp_file)
         return temp_file.name
 
 
-def generate_schema_types(input_data: Dict, output_location: str):
-    """ takes in a Dict representing a schema as input then appends the resulting python code to the output file
-    """
+def generate_schema_types(input_data: Dict):
+    """takes in a Dict representing a schema as input then appends the resulting python code to the output file"""
     tmp_input = _temp_store_input_data(input_data)
+    tmp_output = tempfile.NamedTemporaryFile(
+        mode="w", delete=False, prefix="polyapi_", suffix=".json"
+    ).name
+
     config: configuration.Configuration = {
         "python_version": None,  # type: ignore
         "generate": [
             {
                 "source": tmp_input,
-                "destination": output_location,
+                "destination": tmp_output,
             }
         ],
     }
     process_config(config)
+
+    with open(tmp_output) as f:
+        output = f.read()
+
+    return output
