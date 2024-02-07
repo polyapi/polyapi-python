22c0e6ecceba7b91a578a7123d8c45a350d3fad5
Dan Fellin <dan@highwaterlabs.com>
2024-02-07 11:24:42 -0800

    nice v1 for just getToken

--- polyapi/auth.py
+++ polyapi/auth.py
@@ -1,6 +1,27 @@
 from typing import List, Dict, Any, Tuple
 
 from polyapi.typedefs import PropertySpecification
+from polyapi.utils import parse_arguments, get_type_and_def
+
+
+AUTH_DEFS_TEMPLATE = """
+from typing import List, Dict, Any, TypedDict
+{args_def}
+{return_type_def}
+"""
+
+GET_TOKEN_TEMPLATE = """
+def getToken(clientId, clientSecret, scopes, callback, userId) -> Dict:
+    url = "/auth-providers/{function_id}/execute"
+    resp = execute_post(url, {{
+        "clientId": clientId,
+        "clientSecret": clientSecret,
+        "scopes": scopes,
+        "userId": userId,
+    }})
+    data = resp.json()
+    return callback(data.get("token"), data.get("url"), data.get("error"))
+"""
 
 
 def render_auth_function(
@@ -11,7 +32,18 @@ def render_auth_function(
     arguments: List[PropertySpecification],
     return_type: Dict[str, Any],
 ) -> Tuple[str, str]:
-    print(f"TODO add auth function {function_name}!")
+    """ renders getToken, revokeToken, refreshToken as appropriate
+    """
+    args, args_def = parse_arguments(function_name, arguments)
+    return_type_name, return_type_def = get_type_and_def(return_type)  # type: ignore
+    func_type_defs = AUTH_DEFS_TEMPLATE.format(
+        args_def=args_def,
+        return_type_def=return_type_def,
+    )
+
     func_str = ""
-    func_type_defs = ""
+
+    if function_name == "getToken":
+        func_str = GET_TOKEN_TEMPLATE.format(function_id=function_id)
+
     return func_str, func_type_defs
