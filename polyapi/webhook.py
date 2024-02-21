02f2a2cd3475e888859ebe238d6f680edcf96a96
Dan Fellin <dan@highwaterlabs.com>
2024-02-21 12:56:02 -0800

    add support for waitForResponse for webhooks

--- polyapi/webhook.py
+++ polyapi/webhook.py
@@ -21,15 +21,29 @@ async def {function_name}(callback, options=None):
         else:
             print("Could not set register webhook event handler for {function_id}")
 
-    def handleEvent(data):
+    async def handleEvent(data):
+        nonlocal api_key
         nonlocal options
         polyCustom = {{}}
-        callback(data.get("body"), data.get("headers"), data.get("params"), polyCustom)
+        resp = await callback(data.get("body"), data.get("headers"), data.get("params"), polyCustom)
+        if resp and options.get("waitForResponse"):
+            await socket.emit('setWebhookListenerResponse', {{
+                "webhookHandleID": function_id,
+                "apiKey": api_key,
+                "clientID": eventsClientId,
+                "executionId": data.get("executionId"),
+                "response": {{
+                    "data": resp,
+                    "statusCode": polyCustom.get("responseStatusCode", 200),
+                    "contentType": polyCustom.get("responseContentType", None),
+                }},
+            }}, namespace="/events")
 
     data = {{
         "clientID": eventsClientId,
         "webhookHandleID": function_id,
-        "apiKey": api_key
+        "apiKey": api_key,
+        "waitForResponse": options.get("waitForResponse"),
     }}
     await socket.emit('registerWebhookEventHandler', data, namespace="/events", callback=registerCallback)
 
