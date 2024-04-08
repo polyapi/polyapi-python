9ee4b83ef8d042bc32cf884bbe9394d9f07629c6
Dan Fellin <dan@highwaterlabs.com>
2024-04-08 08:16:06 -0700

    add error_handler

---
+++ polyapi/error_handler.py
@@ -0,0 +1,45 @@
+import copy
+import socketio  # type: ignore
+from typing import Any, Callable, Dict, Optional
+
+from polyapi.config import get_api_key_and_url
+
+
+local_error_handlers: Dict[str, Any] = {}
+
+
+async def on(path: str, callback: Callable, options: Optional[Dict[str, Any]]) -> Callable:
+    assert not local_error_handlers
+    socket = socketio.AsyncClient()
+    api_key, base_url = get_api_key_and_url()
+    await socket.connect(base_url, transports=["websocket"], namespaces=["/events"])
+
+    handler_id = None
+    data = copy.deepcopy(options or {})
+    data["path"] = path
+    data["apiKey"] = api_key
+
+    def registerCallback(id: int):
+        nonlocal handler_id, socket
+        handler_id = id
+        socket.on(f"handleError:{handler_id}", callback, namespace="/events")
+
+    socket.emit("registerErrorHandler", data, registerCallback)
+    if local_error_handlers.get(path):
+        local_error_handlers[path].append(callback)
+    else:
+        local_error_handlers[path] = [callback]
+
+    async def unregister():
+        nonlocal handler_id, socket
+        if handler_id and socket:
+            await socket.emit(
+                "unregisterErrorHandler",
+                {"id": handler_id, "path": path, "apiKey": api_key},
+                namespace="/events",
+            )
+
+        if local_error_handlers.get(path):
+            local_error_handlers[path].remove(callback)
+
+    return unregister
