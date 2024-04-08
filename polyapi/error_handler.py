1dd6e8e7448fb15eda2ced51a309319be3afa846
Dan Fellin <dan@highwaterlabs.com>
2024-04-08 09:23:28 -0700

    add error_handler

--- polyapi/error_handler.py
+++ polyapi/error_handler.py
@@ -1,3 +1,4 @@
+import asyncio
 import copy
 import socketio  # type: ignore
 from typing import Any, Callable, Dict, Optional
@@ -8,38 +9,44 @@ from polyapi.config import get_api_key_and_url
 local_error_handlers: Dict[str, Any] = {}
 
 
-async def on(path: str, callback: Callable, options: Optional[Dict[str, Any]]) -> Callable:
+def on(path: str, callback: Callable, options: Optional[Dict[str, Any]] = None) -> Callable:
     assert not local_error_handlers
     socket = socketio.AsyncClient()
     api_key, base_url = get_api_key_and_url()
-    await socket.connect(base_url, transports=["websocket"], namespaces=["/events"])
 
-    handler_id = None
-    data = copy.deepcopy(options or {})
-    data["path"] = path
-    data["apiKey"] = api_key
+    async def _inner():
+        await socket.connect(base_url, transports=["websocket"], namespaces=["/events"])
 
-    def registerCallback(id: int):
-        nonlocal handler_id, socket
-        handler_id = id
-        socket.on(f"handleError:{handler_id}", callback, namespace="/events")
+        handler_id = None
+        data = copy.deepcopy(options or {})
+        data["path"] = path
+        data["apiKey"] = api_key
 
-    socket.emit("registerErrorHandler", data, registerCallback)
-    if local_error_handlers.get(path):
-        local_error_handlers[path].append(callback)
-    else:
-        local_error_handlers[path] = [callback]
+        def registerCallback(id: int):
+            nonlocal handler_id, socket
+            handler_id = id
+            socket.on(f"handleError:{handler_id}", callback, namespace="/events")
 
-    async def unregister():
-        nonlocal handler_id, socket
-        if handler_id and socket:
-            await socket.emit(
-                "unregisterErrorHandler",
-                {"id": handler_id, "path": path, "apiKey": api_key},
-                namespace="/events",
-            )
-
+        await socket.emit("registerErrorHandler", data, "/events", registerCallback)
         if local_error_handlers.get(path):
-            local_error_handlers[path].remove(callback)
+            local_error_handlers[path].append(callback)
+        else:
+            local_error_handlers[path] = [callback]
 
-    return unregister
+        async def unregister():
+            nonlocal handler_id, socket
+            if handler_id and socket:
+                await socket.emit(
+                    "unregisterErrorHandler",
+                    {"id": handler_id, "path": path, "apiKey": api_key},
+                    namespace="/events",
+                )
+
+            if local_error_handlers.get(path):
+                local_error_handlers[path].remove(callback)
+
+        await socket.wait()
+
+        return unregister
+
+    return asyncio.run(_inner())
