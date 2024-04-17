0d634757f2c021cd6338d4290d9446ad5e5d45a8
Dan Fellin <dan@highwaterlabs.com>
2024-04-17 12:05:47 -0700

    add ability to do multiple error handlers!

--- polyapi/error_handler.py
+++ polyapi/error_handler.py
@@ -1,52 +1,78 @@
 import asyncio
 import copy
 import socketio  # type: ignore
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 from polyapi.config import get_api_key_and_url
 
 
-local_error_handlers: Dict[str, Any] = {}
+active_handlers: List[Dict[str, Any]] = []
+client = None
 
 
-def on(path: str, callback: Callable, options: Optional[Dict[str, Any]] = None) -> Callable:
-    assert not local_error_handlers
-    socket = socketio.AsyncClient()
-    api_key, base_url = get_api_key_and_url()
+def prepare():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_client_and_connect())
+    print("Client initialized!")
 
-    async def _inner():
-        await socket.connect(base_url, transports=["websocket"], namespaces=["/events"])
 
-        handler_id = None
-        data = copy.deepcopy(options or {})
-        data["path"] = path
-        data["apiKey"] = api_key
 
-        def registerCallback(id: int):
-            nonlocal handler_id, socket
-            handler_id = id
-            socket.on(f"handleError:{handler_id}", callback, namespace="/events")
+async def get_client_and_connect():
+    _, base_url = get_api_key_and_url()
+    global client
+    client = socketio.AsyncClient()
+    await client.connect(base_url, transports=["websocket"], namespaces=["/events"])
 
-        await socket.emit("registerErrorHandler", data, "/events", registerCallback)
-        if local_error_handlers.get(path):
-            local_error_handlers[path].append(callback)
-        else:
-            local_error_handlers[path] = [callback]
 
-        async def unregister():
-            nonlocal handler_id, socket
-            if handler_id and socket:
-                await socket.emit(
-                    "unregisterErrorHandler",
-                    {"id": handler_id, "path": path, "apiKey": api_key},
-                    namespace="/events",
-                )
+async def unregister(data: Dict[str, Any]):
+    print(f"stopping error handler for '{data['path']}'...")
+    assert client
+    await client.emit(
+        "unregisterErrorHandler",
+        data,
+        "/events",
+    )
 
-            if local_error_handlers.get(path):
-                local_error_handlers[path].remove(callback)
 
-        await socket.wait()
+async def unregister_all():
+    _, base_url = get_api_key_and_url()
+    # need to reconnect because maybe socketio client disconnected after Ctrl+C?
+    await client.connect(base_url, transports=["websocket"], namespaces=["/events"])
+    await asyncio.gather(*[unregister(handler) for handler in active_handlers])
 
-        return unregister
 
-    return asyncio.run(_inner())
+async def on(
+    path: str, callback: Callable, options: Optional[Dict[str, Any]] = None
+) -> None:
+    print(f"starting error handler for {path}...")
+
+    if not client:
+        raise Exception("Client not initialized. Please call error_handler.prepare() first.")
+
+    api_key, _ = get_api_key_and_url()
+    handler_id = None
+    data = copy.deepcopy(options or {})
+    data["path"] = path
+    data["apiKey"] = api_key
+
+    def registerCallback(id: int):
+        nonlocal handler_id
+        handler_id = id
+        client.on(f"handleError:{handler_id}", callback, namespace="/events")
+        active_handlers.append({"path": path, "id": handler_id, "apiKey": api_key})
+
+    await client.emit("registerErrorHandler", data, "/events", registerCallback)
+
+
+def start(*args):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_client_and_connect())
+    asyncio.gather(*args)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(unregister_all())
+        loop.stop()
