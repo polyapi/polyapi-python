b8fb57de202c10bcb802944e80697f956e4351fa
Ashir Rao <69091220+Ash1R@users.noreply.github.com>
2025-06-25 14:34:36 -0700

    using mkdir to avoid poly not existing

--- polyapi/__init__.py
+++ polyapi/__init__.py
@@ -10,11 +10,11 @@ from .cli import CLI_COMMANDS
 __all__ = ["poly"]
 
 
-# if len(sys.argv) > 1 and sys.argv[1] not in CLI_COMMANDS:
-#     currdir = os.path.dirname(os.path.abspath(__file__))
-#     if not os.path.isdir(os.path.join(currdir, "poly")):
-#         print("No 'poly' found. Please run 'python3 -m polyapi generate' to generate the 'poly' library for your tenant.")
-#         sys.exit(1)
+if len(sys.argv) > 1 and sys.argv[1] not in CLI_COMMANDS:
+    currdir = os.path.dirname(os.path.abspath(__file__))
+    if not os.path.isdir(os.path.join(currdir, "poly")):
+        print("No 'poly' found. Please run 'python3 -m polyapi generate' to generate the 'poly' library for your tenant.")
+        sys.exit(1)
 
 
 class PolyCustomDict(TypedDict, total=False):
