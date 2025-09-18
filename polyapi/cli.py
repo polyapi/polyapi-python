479c2707b162b2db3ff3924fdd183b6c957b4a34
eric.neumann <neumann.bend@gmail.com>
2025-09-17 17:02:15 -0700

    EN #5300 match env var names with TS client

--- polyapi/cli.py
+++ polyapi/cli.py
@@ -47,8 +47,8 @@ def execute_from_cli():
     setup_parser.add_argument("url", nargs="?", help="URL for the Poly API")
 
     def setup(args):
-        api_key = args.api_key or os.getenv("DEPLOY_POLY_API_KEY")
-        url = args.url or os.getenv("DEPLOY_POLY_API_BASE_URL")
+        api_key = args.api_key or os.getenv("POLY_API_KEY")
+        url = args.url or os.getenv("POLY_API_BASE_URL")
 
         if api_key and url:
             set_api_key_and_url(api_key, url)
