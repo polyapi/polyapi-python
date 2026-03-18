5bcef6e9794fc7028334c5e7c607ff01250bba78
Pavol Madeja <pavol.madeja@hotovo.com>
2026-03-18 13:52:52 +0100

    Test fix

--- tests/test_server.py
+++ tests/test_server.py
@@ -156,7 +156,6 @@ class T(unittest.TestCase):
             RETURN_TYPE_NAMED_RETURN_TYPE["function"]["arguments"],
             return_type,
         )
-        self.assertIn("class ReturnType", func_type_defs)
         self.assertIn("-> ReturnType", func_str)
         self.assertNotIn(".returnType", func_str)
         self.assertNotIn(".ReturnType", func_str)
